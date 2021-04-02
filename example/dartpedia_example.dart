32ec2ba089a28bba49aeea4e038b615680430b4e
flikkr <kazymir.rabier@gmail.com>
2021-04-02 17:15:33 +0800

    fixed example

--- example/dartpedia_example.dart
+++ example/dartpedia_example.dart
@@ -1,5 +1,14 @@
 import 'package:dartpedia/dartpedia.dart';
 
-void main() {
-  var res = Dartpedia.random();
+Future<void> main() async {
+  var res = await Dartpedia.searchQuery('Google');
+  var pageid = res?.results?.first.pageId;
+
+  if (pageid != null) {
+    var google = await Dartpedia.summary(pageid);
+
+    print(google?.title); // Returns "Google"
+    print(google?.description); // Returns "American technology company"
+    print(google?.extract); // Returns "Google LLC is an American multinational technology company that specializes in Internet-related..."
+  }
 }
